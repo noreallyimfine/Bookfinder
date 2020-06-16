9a181d89c1af8dd28fbbdb2f28fe6b9041d661b1
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-06-16 11:39:11 -0400

    removed extra comments and prints

--- book_list_scrape.py
+++ book_list_scrape.py
@@ -9,18 +9,13 @@ f = open("best_books_titles.csv", 'w', encoding='UTF-8')
 csv_writer = csv.writer(f)
 csv_writer.writerow(["Title", "Author", "Rating"])
 titles = []
-for page in range(1, 5):
+for page in range(1, 524):
     driver.get(f'{BASE_URL}{BOOK_LIST}{page}')
     for i in range(1, 101):
         title = driver.find_element_by_xpath(f'//*[@id="all_votes"]/table/tbody/tr[{i}]/td[3]')
         title_list = title.text.split("\n")
         csv_writer.writerow(title_list)
-# print(dir(title))
-# Harry Potter and the Order of the Phoenix -> //*[@id="all_votes"]/table/tbody/tr[2]/td[3]
-# The Hunger Games -> //*[@id="all_votes"]/table/tbody/tr[1]/td[3]
-# Moby Dick (page 2) -> //*[@id="all_votes"]/table/tbody/tr[1]/td[3]
-f.close()
-hunger_games = titles[0]
 
-print(hunger_games.text.split('\n'))
+
+f.close()
 driver.close()
