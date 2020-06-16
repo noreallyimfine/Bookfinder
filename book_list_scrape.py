303b144ad0fbe197f8a4e6a96ddad368508be82b
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-06-16 09:11:47 -0400

    add base_url and first page tester

--- book_list_scrape.py
+++ book_list_scrape.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
 
 driver = webdriver.Chrome()
+BASE_URL = "https://www.goodreads.com/list/show/"
 
-driver.get("https://www.google.com")
+first_page = "1.Best_Books_Ever"
+driver.get(BASE_URL + first_page)
+
+
 
 driver.close()
