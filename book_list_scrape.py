3844baeb9018e0f8d278fe532c6ecd966b77f6a7
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-06-16 09:39:33 -0400

    #workingIt

--- book_list_scrape.py
+++ book_list_scrape.py
@@ -6,6 +6,7 @@ BASE_URL = "https://www.goodreads.com/list/show/"
 first_page = "1.Best_Books_Ever"
 driver.get(BASE_URL + first_page)
 
+title = driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[1]/div[2]/div[3]/div[5]/table/tbody/tr[1]/td[3]/a/span')
+print(title)
 
-
-# driver.close()
+driver.close()
