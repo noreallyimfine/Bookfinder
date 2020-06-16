c27b76d5385a7acca854ccf4c1477bd2685e1a29
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-06-16 10:24:21 -0400

    pull list of titles from multiple pages with relevant author and rating info and write to csv

--- book_list_scrape.py
+++ book_list_scrape.py
@@ -1,18 +1,25 @@
 from selenium import webdriver
+import csv
 
 driver = webdriver.Chrome()
 BASE_URL = "https://www.goodreads.com/list/show/"
+BOOK_LIST = "1.Best_Books_Ever?page="
 
-first_page = "1.Best_Books_Ever"
-driver.get(BASE_URL + first_page)
-
+f = open("best_books_titles.csv", 'w', encoding='UTF-8')
+csv_writer = csv.writer(f)
+csv_writer.writerow(["Title", "Author", "Rating"])
 titles = []
-for i in range(1, 101):
-    title = driver.find_element_by_xpath(f'//*[@id="all_votes"]/table/tbody/tr[{i}]/td[3]')
-    titles.append(title)
+for page in range(1, 5):
+    driver.get(f'{BASE_URL}{BOOK_LIST}{page}')
+    for i in range(1, 101):
+        title = driver.find_element_by_xpath(f'//*[@id="all_votes"]/table/tbody/tr[{i}]/td[3]')
+        title_list = title.text.split("\n")
+        csv_writer.writerow(title_list)
 # print(dir(title))
 # Harry Potter and the Order of the Phoenix -> //*[@id="all_votes"]/table/tbody/tr[2]/td[3]
 # The Hunger Games -> //*[@id="all_votes"]/table/tbody/tr[1]/td[3]
+# Moby Dick (page 2) -> //*[@id="all_votes"]/table/tbody/tr[1]/td[3]
+f.close()
 hunger_games = titles[0]
 
 print(hunger_games.text.split('\n'))
