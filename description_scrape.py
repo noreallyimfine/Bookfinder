f21bc7c0d7f092a357e28d664602c60c6f241fef
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-06-17 09:37:58 -0400

    testing out clicking and going back

--- description_scrape.py
+++ description_scrape.py
@@ -1,3 +1,14 @@
 from selenium import webdriver
 import csv
 
+driver = webdriver.Chrome()
+
+BASE_URL = "https://www.goodreads.com/list/show/"
+BOOK_LIST = "1.Best_Books_Ever?page="
+
+# f = open("best_books_details.csv", 'w', encoding='UTF-8')
+
+driver.get(f'{BASE_URL}{BOOK_LIST}1')
+
+for i in range(1, 101):
+
