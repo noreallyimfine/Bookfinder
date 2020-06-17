1eb13b061b290185b538a01d7b5bfc8fdf41a02e
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-06-17 10:25:04 -0400

    comment out extra steps for test of handle_signin

--- description_scrape.py
+++ description_scrape.py
@@ -22,12 +22,20 @@ driver.get(f'{BASE_URL}{BOOK_LIST}1')
 
 for i in range(1, 5):
 
+    handle_signin()
+
     title = driver.find_element_by_xpath(f'//*[@id="all_votes"]/table/tbody/tr[{i}]/td[3]/a/span')
     author = driver.find_element_by_xpath(f'//*[@id="all_votes"]/table/tbody/tr[{i}]/td[3]/span[2]')
     rating = driver.find_element_by_xpath(f'//*[@id="all_votes"]/table/tbody/tr[{i}]/td[3]/div[1]/span/span')
     
     title.click()
-    time.sleep(3)
+    handle_signin()
+
+    # language = driver.find_element_by_xpath()
+
+    # if language.text == 'English':
+    time.sleep(2)
+
     driver.back()
 
 
