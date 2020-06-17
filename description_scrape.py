3cb99f0c2d3427800ae7f602f12aca1d2d64b9e7
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-06-17 11:16:59 -0400

    tryna get to that language div still

--- description_scrape.py
+++ description_scrape.py
@@ -13,28 +13,38 @@ BOOK_LIST = "1.Best_Books_Ever?page="
 
 def handle_signin():
     try:
-        driver.find_element_by_xpath('/html/body/div[4]/div/div/div[1]/button/img').click()
-    except NoSuchElementException:
+        driver.find_element_by_xpath('/html/body/div[3]/div/div/div[1]/button').click()
+        print("Found element and clicked!")
+    except NoSuchElementException as e:
+        print(e)
+    finally:
         return
 
 
 driver.get(f'{BASE_URL}{BOOK_LIST}1')
 
+counter = 0
 for i in range(1, 5):
 
-    handle_signin()
+    time.sleep(1)
+    if counter < 3:
+        handle_signin()
 
     title = driver.find_element_by_xpath(f'//*[@id="all_votes"]/table/tbody/tr[{i}]/td[3]/a/span')
     author = driver.find_element_by_xpath(f'//*[@id="all_votes"]/table/tbody/tr[{i}]/td[3]/span[2]')
     rating = driver.find_element_by_xpath(f'//*[@id="all_votes"]/table/tbody/tr[{i}]/td[3]/div[1]/span/span')
     
     title.click()
-    handle_signin()
+    time.sleep(1)
+    if counter < 3:
+        handle_signin()
+    
+    # open more details
+    driver.find_element_by_xpath('//*[@id="details"]/div[3]').click()
 
-    # language = driver.find_element_by_xpath()
+    language = driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[1]/div[2]/div[4]/div[1]/div[2]/div[5]/div[3]/div[1]/div[3]/div[2]')
 
-    # if language.text == 'English':
-    time.sleep(2)
+    print("Language:", language.text)
 
     driver.back()
 
