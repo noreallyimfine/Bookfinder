f943b3d54751067993966db94c1555d67e9e1e7d
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-06-19 10:50:23 -0400

    create dict for writing values

--- book_list_scrape.py
+++ book_list_scrape.py
@@ -12,6 +12,12 @@ titles = []
 for page in range(1, 525):
     driver.get(f'{BASE_URL}{BOOK_LIST}{page}')
     for i in range(1, 101):
+
+        # dictionary to write consistently to csv
+        # should start with keys pointint to Nones
+        # there will be check to make sure the value is being written to the right key
+        # can write Nones so we dont get bad data
+
         title = driver.find_element_by_xpath(f'//*[@id="all_votes"]/table/tbody/tr[{i}]/td[3]/a/span')
         author = driver.find_element_by_xpath(f'//*[@id="all_votes"]/table/tbody/tr[{i}]/td[3]/span[2]')
         rating = driver.find_element_by_xpath(f'//*[@id="all_votes"]/table/tbody/tr[{i}]/td[3]/div[1]/span/span')
--- description_scrape.py
+++ description_scrape.py
@@ -29,15 +29,28 @@ counter = 0
 for page in range(1, 11):
     driver.get(f'{BASE_URL}{BOOK_LIST}{page}')
 
-    for i in range(1, 5):
+    for i in range(1, 101):
+        
+        
+        # dictionary to write consistently to csv
+        # should start with keys pointint to Nones
+        # there will be check to make sure the value is being written to the right key
+        # can write Nones so we dont get bad data
+        data = {"title": None,
+                "author": None,
+                "rating": None,
+                "description": None,
+                "Language Edition": None,
+                "Language": None,
+                "ISBN": None}
 
         time.sleep(1)
         if counter < 3:
             counter = handle_signin(counter)
 
-        title = driver.find_element_by_xpath(f'//*[@id="all_votes"]/table/tbody/tr[{i}]/td[3]/a/span')
-        author = driver.find_element_by_xpath(f'//*[@id="all_votes"]/table/tbody/tr[{i}]/td[3]/span[2]')
-        rating = driver.find_element_by_xpath(f'//*[@id="all_votes"]/table/tbody/tr[{i}]/td[3]/div[1]/span/span')
+        data["title"] = driver.find_element_by_xpath(f'//*[@id="all_votes"]/table/tbody/tr[{i}]/td[3]/a/span')
+        data["author"] = driver.find_element_by_xpath(f'//*[@id="all_votes"]/table/tbody/tr[{i}]/td[3]/span[2]')
+        data["rating"] = driver.find_element_by_xpath(f'//*[@id="all_votes"]/table/tbody/tr[{i}]/td[3]/div[1]/span/span')
 
         title.click()
         time.sleep(.5)
